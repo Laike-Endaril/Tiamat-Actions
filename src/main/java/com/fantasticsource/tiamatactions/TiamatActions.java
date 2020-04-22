0aee171f8921f04dc8e33b8630ecf97643b2a3e5
Kyle Koder <silvertallon@gmail.com>
2020-04-21 21:33:23 -0700

    1.12.2.000a; fixed color related crash, presumably from old lib version

--- src/main/java/com/fantasticsource/tiamatactions/TiamatActions.java
+++ src/main/java/com/fantasticsource/tiamatactions/TiamatActions.java
@@ -13,7 +13,7 @@ public class TiamatActions
 {
     public static final String MODID = "tiamatactions";
     public static final String NAME = "Tiamat Actions";
-    public static final String VERSION = "1.12.2.000";
+    public static final String VERSION = "1.12.2.000a";
 
 
     @Mod.EventHandler
