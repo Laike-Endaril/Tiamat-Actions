62bf8e5a57d38190604a651215e8c9eb8189797c
Kyle Koder <silvertallon@gmail.com>
2020-06-23 14:29:24 -0700

    Updated lib

--- src/main/java/com/fantasticsource/tiamatactions/TiamatActions.java
+++ src/main/java/com/fantasticsource/tiamatactions/TiamatActions.java
@@ -20,7 +20,7 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import java.util.List;
 
-@Mod(modid = TiamatActions.MODID, name = TiamatActions.NAME, version = TiamatActions.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.034y,)")
+@Mod(modid = TiamatActions.MODID, name = TiamatActions.NAME, version = TiamatActions.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.034z,)")
 public class TiamatActions
 {
     public static final String MODID = "tiamatactions";
