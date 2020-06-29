55683ffb3dc99d9efc6e592d2237b3f4eb1e87ef
Kyle Koder <silvertallon@gmail.com>
2020-06-29 14:10:36 -0700

    1.12.2.000d

--- src/main/java/com/fantasticsource/tiamatactions/TiamatActions.java
+++ src/main/java/com/fantasticsource/tiamatactions/TiamatActions.java
@@ -21,7 +21,7 @@ public class TiamatActions
 {
     public static final String MODID = "tiamatactions";
     public static final String NAME = "Tiamat Actions";
-    public static final String VERSION = "1.12.2.000c";
+    public static final String VERSION = "1.12.2.000d";
 
 
     private static final ScriptEngineManager SCRIPT_ENGINE_MANAGER = new ScriptEngineManager();
