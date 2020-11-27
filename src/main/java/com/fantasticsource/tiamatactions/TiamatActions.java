60fe2779d9000fd1ce5aa44c4f3c78f4b529c4dc
Kyle Koder <silvertallon@gmail.com>
2020-11-27 00:05:24 -0800

    1.12.2.000zz

--- src/main/java/com/fantasticsource/tiamatactions/TiamatActions.java
+++ src/main/java/com/fantasticsource/tiamatactions/TiamatActions.java
@@ -23,7 +23,7 @@ public class TiamatActions
 {
     public static final String MODID = "tiamatactions";
     public static final String NAME = "Tiamat Actions";
-    public static final String VERSION = "1.12.2.000zy";
+    public static final String VERSION = "1.12.2.000zz";
     public static final String DOMAIN = "tiamatrpg";
 
 
